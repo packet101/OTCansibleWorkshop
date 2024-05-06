3a8b4bb622ba4f722d45f7b6b8c1d2907502841c
Ian Logan <ilogan@one-technology.com>
2024-05-06 11:49:23 -0600

    python venv

---
+++ setup.sh
@@ -0,0 +1,8 @@
+#!/usr/bin/env bash
+
+python -m venv ~/ansible-workshop
+
+source ~/ansible-workshop/bin/activate
+pip install -r requirements.txt
+
+echo "source ~/ansible-workshop/bin/activate" >> ~/.zshrc
